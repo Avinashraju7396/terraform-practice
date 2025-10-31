15c24304f39bf43a34c11deaedbb270bdcc02a27
unknown <avinashraju66@gmail.com>
2025-10-31 14:29:57 +0530

    ching1

--- day-4-state-locking/backend.tf
+++ day-4-state-locking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "kjhgfdfghjklkmjnhb"   #create a new bucket
     key    = "day-4/terraform.tfstate"
+    use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-east-1" 
   }
 }
