4b817258b029bf7c587cbe7addaa0fe3cfe55845
Siddapuram <avinashraju66@gmail.com>
2025-11-01 16:20:47 +0530

    Update main.tf

--- day-6 import/main.tf
+++ day-6 import/main.tf
@@ -1,9 +1,18 @@
 resource "aws_instance" "name" {
-    ami = "ami-0e6be795b21969e1d"
+    ami = "i-04a9ef2c1c2923605"
     instance_type = "t3.micro"
     tags = {
       Name = "ec2"
     }
   
 }
+
+#terraform import aws_instance.name i-04a9ef2c1c2923605  chnage instance id that already exit 
+
+resource "aws_s3_bucket" "name" {
+    bucket = "avinash0123"
+  
+}
+#terraform import aws_s3_bucket.name avinash0123   here change bucket name that already exist
+
 
