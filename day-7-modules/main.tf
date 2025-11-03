0ab57e35cf564a7c3288017edf26891f3acda3cf
unknown <avinashraju66@gmail.com>
2025-11-03 14:22:33 +0530

    modules

--- day-7-modules/main.tf
+++ day-7-modules/main.tf
@@ -1,5 +1,5 @@
 module "name" {
-   source = "../day-2-basic-code"
+   source = "../day-2-basic-code"   #added day-2 update only instance
    ami_id = "ami-07860a2d7eb515d9a"
    type = "t3.micro"
 }
