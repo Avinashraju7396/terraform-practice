8e914f5c2ba50c65e61c9672f89e0d8ef64df7a4
unknown <avinashraju66@gmail.com>
2025-11-12 18:52:31 +0530

    day9

---
+++ day-9-keymanagment/main.tf
@@ -0,0 +1,16 @@
+
+# ✅ Import existing SSH public key from your system
+resource "aws_key_pair" "example" {
+  key_name   = "task"
+  public_key = file("C:/Users/DELL/.ssh/id_ed25519.pub")   # <-- UPDATE this path
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-0cae6d6fe6048ca2c"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.example.key_name
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
