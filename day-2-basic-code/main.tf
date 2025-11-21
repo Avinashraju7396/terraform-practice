822f01bac723ae9a66167af2b0f69afb824c3792
Siddapuram <avinashraju66@gmail.com>
2025-11-21 15:16:14 +0530

    Update main.tf

--- day-2-basic-code/main.tf
+++ day-2-basic-code/main.tf
@@ -1,25 +1,32 @@
-resource "aws_instance" "name" { 
-    instance_type = var.type
-     ami = var.ami_id
-     subnet_id = aws_subnet.name.id
-     tags = {
-       Name = "test"
-     }
-
-
-  
-}
-
 resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-  
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-test"
+  }
 }
+
 resource "aws_subnet" "name" {
-  vpc_id = aws_vpc.name.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.name.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true  # important to get public IP
+
   tags = {
     Name = "subnet-1"
   }
 }
 
- 
+resource "aws_instance" "name" { 
+  ami           = var.ami_id
+  instance_type = var.type
+  subnet_id     = aws_subnet.name.id
+
+  associate_public_ip_address = true  # important for SSH
+
+  tags = {
+    Name = "test"
+  }
+}
